9e87931e7f6b719cb2a50784caa71a29c64bfd2e
SigitasM <s.milkamanavicius@gmail.com>
2020-07-26 21:53:32 +0300

    Add delete ToDo functionality

--- todo_app/todo_items/urls.py
+++ todo_app/todo_items/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='list_page'),
-    path('update_todo/<str:pk>/', views.updateTodo, name='update_todo')
+    path('update_todo/<str:pk>/', views.updateTodo, name='update_todo'),
+    path('delete_todo/<str:pk>/', views.deleteTodo, name='delete_todo'),
 ]
