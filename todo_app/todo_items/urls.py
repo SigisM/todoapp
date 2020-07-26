f533ea869390ce3fb39d89eb004c226ab426cca1
SigitasM <s.milkamanavicius@gmail.com>
2020-07-26 15:51:36 +0300

    Update ToDo features

--- todo_app/todo_items/urls.py
+++ todo_app/todo_items/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index_page'),
+    path('', views.index, name='list_page'),
+    path('update_todo/<str:pk>/', views.updateTodo, name='update_todo')
 ]
