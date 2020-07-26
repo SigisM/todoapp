bfb686bd34dee63d48e6307653174926e4bc7edf
SigitasM <s.milkamanavicius@gmail.com>
2020-07-26 13:39:47 +0300

    Add ToDo and Shopping list pages

---
+++ todo_app/shopping_items/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ShoppingItemsConfig(AppConfig):
+    name = 'shopping_items'
--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '==(e5t&5%oc*e0s=%lgc5br^g3ger*j*ubv%!wa@wv5m_p%55q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo_items',
+    'shopping_items',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = (
+#      os.path.join(BASE_DIR, 'static'),
+# )
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
