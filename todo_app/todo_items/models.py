49d6cc59edd61acfe6e6db6662dc020c23f3dbea
SigitasM <s.milkamanavicius@gmail.com>
2020-08-08 10:03:02 +0300

    Various fix in template\

--- todo_app/todo_items/models.py
+++ todo_app/todo_items/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+
 
 class Todo(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateField(default=datetime.date.today)
 
     def __str__(self):
         return self.title
