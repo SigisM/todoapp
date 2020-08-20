821dfc6625026db4a1b20a7ba4f4151a19612260
SigitasM <s.milkamanavicius@gmail.com>
2020-08-20 23:00:47 +0300

    Delete unused/commented code, delete unused imports

--- todo_app/todo_items/tasks.py
+++ todo_app/todo_items/tasks.py
@@ -14,16 +14,3 @@ def send_email_task3():
     'my.todo.apl@gmail.com',
     ['s.milkamanavicius@gmail.com'])
     return None
-
-
-logger = get_task_logger(__name__)
-
-
-# @periodic_task(options={"task_id": "my_periodic_task"}, run_every=(crontab(minute=reminder(reminder_minute), hour=reminder(reminder_hour))), name="send_email_task", ignore_result=True)
-def send_email_task2():
-
-    send_mail('Task created!',
-    'Congratulations. You have created a New Task',
-    'my.todo.apl@gmail.com',
-    ['s.milkamanavicius@gmail.com'])
-    return None
