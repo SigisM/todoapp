1008a76a3c512056116cfdb9f7f66603392a22ae
SigitasM <s.milkamanavicius@gmail.com>
2020-08-23 13:32:25 +0300

    Unique task groups for individual users. 1 default task group for every new registered user

--- todo_app/todo_items/urls.py
+++ todo_app/todo_items/urls.py
@@ -6,10 +6,11 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.index, name='list_page'),
+    path('groups/', views.list_group, name='groups_page'),
     path('today/', views.today, name='today_page'),
     path('seven_days/', views.seven_days, name='seven_days_page'),
     path('register/', views.register, name='register_page'),
-    path('update_todo_expand.html/<str:pk>/', views.updateTodo, name='update_todo'),
+    path('update_todo_expand/<str:pk>/', views.updateTodo, name='update_todo'),
     path('delete_todo/<str:pk>/', views.deleteTodo, name='delete_todo'),
 ]
 
