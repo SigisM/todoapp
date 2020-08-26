ebad15894b818aae14886ba349c4892e6533bb44
SigitasM <s.milkamanavicius@gmail.com>
2020-08-26 21:37:41 +0300

    Date fix in entire code. Datetime pick instead of global variables

--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -136,7 +136,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'lt'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'Europe/Vilnius'
 
